878ac8e6b87c8b634627ef33ac8e940dba2178e3
UncleSunQvQ <hao19971002@qq.com>
2021-06-29 11:21:47 +0800

    第一次提交

---
+++ demo01.py
@@ -0,0 +1,20 @@
+'''
+print ('hello world!') #字符串
+print (2) #整数
+print (2.2) #小数
+print (2<3) #布尔值
+print ('拼'+'接') #字符串拼接
+print (23333,55555) #打印多条
+print (9-8+1*(1+1))
+'''
+
+a = input('请输入:')
+b = input('请输入:')
+print('长度和为:',len(a)+len(b))
+
+# print(type('ha'))
+# print(type(1))
+# print(type(1.1))
+# print(type(()))
+# print(type([]))
+# print(type({}))
