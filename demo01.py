16f85c2ec3bda5c5f61e83158ca4d32ea8fdcdd2
UncleSunQvQ <hao19971002@qq.com>
2021-06-29 15:47:44 +0800

    2

--- demo01.py
+++ demo01.py
@@ -7,6 +7,10 @@ print ('拼'+'接') #字符串拼接
 print (23333,55555) #打印多条
 print (9-8+1*(1+1))
 '''
+'''
+所有的方法都是小括号，比如print()  input()
+元组，数组，字典的取值，都是用[]，比如a[8]
+'''
 
 # a = input('请输入:')
 # b = input('请输入:')
@@ -19,8 +23,27 @@ print (9-8+1*(1+1))
 # print(type([]))
 # print(type({}))
 
-a = (1,1.1,'ceshi',1)
+# a = (1,1.1,'ceshi',1)
 # print(a[2])
 
-print(a.index('ceshi'))
-print(a.count(1))
+# print(a.index('ceshi'))
+# print(a.count(1))
+
+# b = (a,'haha','xixi')
+# print(a[0:2]) #左闭右开
+# print(a[2:])
+
+a = [1,1.1,'ceshi',False,True]
+# a.append('append1')
+# a.append('append2')
+# a.insert(0,1)
+# b = a.pop(0) # 剪切
+# c = a.pop(0) # 剪切
+# print(b+c)
+# print(b)
+# a.clear() # 清除
+# xx = ['nihao','buaho']
+# a.extend(xx) #合并数组
+# print(a+xx)
+b = a.count(1)
+print(b)
