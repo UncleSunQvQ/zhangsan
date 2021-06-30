162039a06fb8e63286e4f2b76e7543d4198ce733
UncleSunQvQ <hao19971002@qq.com>
2021-06-30 13:21:30 +0800

    2

--- demo01.py
+++ demo01.py
@@ -79,9 +79,10 @@ print (9-8+1*(1+1))
 # print(a)
 
 
-# name = input('请输入姓名:')
+# name = input('请输入名字:')
 # age = input('请输入年龄:')
 # sex = input('请输入性别:')
 # userinfo = {}
-# userinfo.update(name=name,age=age,sex=sex)
+# userinfo.update(name = name,age = age,sex = sex)
 # print(userinfo)
+
