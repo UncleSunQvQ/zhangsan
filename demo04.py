165f3b8d61c003faec9e76c9500192bd1263f82f
UncleSunQvQ <hao19971002@qq.com>
2021-09-09 15:44:54 +0800

    2

--- demo04.py
+++ demo04.py
@@ -95,12 +95,6 @@
 #             if a**3 + b**3 + c**3 == a*100 + b*10 + c:
 #                 print(a*100+b*10+c)
 
-for a in range(1,10):
-    for b in range(0,10):
-        for c in range(0,10):
-            if a**3 + b**3 + c**3 == a*100 + b*10 + c:
-                print(a*100+b*10+c)
-
 
 # Craps赌博游戏
 # from random import randint
