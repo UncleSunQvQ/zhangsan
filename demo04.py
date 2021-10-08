cba5bdd5dbea6054d024de73331d86a8dfea6b60
UncleSunQvQ <hao19971002@qq.com>
2021-10-08 16:44:09 +0800

    2

--- demo04.py
+++ demo04.py
@@ -270,14 +270,12 @@
 # import os
 # import time
 
-# content = '北 京 欢 迎 你 为 你 开 天 辟 地           '
+# content = '北 京 欢 迎 你 为 你 开 天 辟 地     '
 # while True:
 #     # Windows清除屏幕上的输出
-#     # os.system('cls')  
-#     # macOS清除屏幕上的输出
-#     os.system('cls')
+#     os.system('cls')  
 #     print(content)
-#     time.sleep(0.3)
+#     time.sleep(0.5)
 #     content = content[1:] + content[0]
 
 
@@ -290,6 +288,14 @@
 # for dian in range(1,7):
 #     print(f'{dian}点共出现{tongji[dian-1]}次')
 
+# 练习
+# from random import randint
+# s = [0] * 6
+# for _ in range(6000):
+#     dianshu = randint(1,6)
+#     s[dianshu-1] += 1
+# for i,j in enumerate(s):
+#     print(f'{i+1}点出现了{j}次')
 
 # enumerate函数
 # items = ['Python', 'Java', 'Go', 'Swift']
@@ -344,3 +350,37 @@
 #     s = [score[index] for score in scores]
 #     avg_score = sum(s) / len(names)
 #     print(f'{course}:{avg_score:.1f}')
+
+
+
+# 设计一个函数返回指定日期是这一年的第几天
+# def is_leap_year(year):
+#     """判断指定的年份是不是闰年，平年返回False，闰年返回True"""
+#     return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+# def which_day(year, month, date):
+#     """计算传入的日期是这一年的第几天
+#     :param year: 年
+#     :param month: 月
+#     :param date: 日
+#     """
+#     # 用嵌套的列表保存平年和闰年每个月的天数
+    # days_of_month = [
+        # [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        # [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # ]
+#     # 布尔值False和True可以转换成整数0和1，因此
+#     # 平年会选中嵌套列表中的第一个列表(2月是28天)
+#     # 闰年会选中嵌套列表中的第二个列表(2月是29天)
+#     days = days_of_month[is_leap_year(year)]
+#     total = 0
+#     for index in range(month - 1):
+#         total += days[index]
+#     return total + date
+
+# print(which_day(1980, 11, 28))    # 333
+# print(which_day(1981, 12, 31))    # 365
+# print(which_day(2018, 1, 1))      # 1
+# print(which_day(2016, 3, 1))      # 61
+
+
